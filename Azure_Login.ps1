a6d7f323b5a80ab9371f17ccbdde7c19aecc1b06
Zach <Fishinrocks@aol.com>
2018-01-11 13:55:39 +0100

    Initial commit

---
+++ Azure_Login.ps1
@@ -0,0 +1,16 @@
+﻿Get-AzureSubscription
+
+#Install AzureRM Module
+Install-Module AzureRM
+http://aka.ms/webpi-azps
+
+# To log in to Azure Resource Manager
+$cred = Get-Credential -Credential Domain\User
+Login-AzureRmAccount -Credential $cred
+
+# To view all subscriptions for your account
+Get-AzureRmSubscription | Select-AzureRmSubscription
+
+# To select a default subscription for your current session.
+$subscription = "NAME OF YOUR SUBSCRIPTION GOES HERE"
+Get-AzureRmSubscription -SubscriptionName $subscription | Select-AzureRmSubscription
